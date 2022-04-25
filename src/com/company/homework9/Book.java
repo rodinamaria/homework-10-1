600df2085cda6edbc0c959d39b82af2de577c7c3
mariarodina <q6m4?Qskk#>
2022-04-26 01:20:19 +0300

    дз 10. Методы объектов.

--- src/com/company/homework9/Book.java
+++ src/com/company/homework9/Book.java
@@ -1,5 +1,7 @@
 package com.company.homework9;
 
+import java.util.Objects;
+
 public class Book {
 
     private final String title;
@@ -29,6 +31,25 @@ public class Book {
         this.year = year;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
 
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author=" + author +
+                ", year=" + year +
+                '}';
+    }
 }
